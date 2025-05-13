b55242a996b3ba5b5e2ddf72ffae81473f14474e
Alexander-HR <alexanderje24@ru.is>
2025-05-13 11:02:00 +0000

    Forgot to change branches but I finished the seller profile

--- vln2/CastleApartments/urls.py
+++ vln2/CastleApartments/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.property_catalog, name='home'),
-    path('profile/', views.buyer_profile, name='buyer_profile'),
+    path('#/', views.buyer_profile, name='buyer_profile'),
     path('property/', views.property_detail, name='property_details'),
+    path('profile/', views.seller_profile, name='seller_profile'),
+
 
 ]
--- vln2/CastleApartments/views.py
+++ vln2/CastleApartments/views.py
@@ -15,6 +15,24 @@ def buyer_profile(request):  #, pk):
     return render(request, 'buyerProfile.html', {'user': user})
     """
 
+def seller_profile(request):  #, pk):
+    page = int(request.GET.get('page', 1))
+    page_size = 16 if page == 1 else 24
+
+    properties = Property.objects.all()  # Later: filter by seller
+    paginator = Paginator(properties, page_size)
+    page_obj = paginator.get_page(page)
+
+    return render(request, 'sellerProfile.html', {
+        'properties': page_obj.object_list,
+        'has_next': page_obj.has_next(),
+        'next_page': page + 1
+    })
+    """
+    userProfile = userProfile.objects.get(pk=pk)
+    return render(request, 'sellerProfile.html', {'user': user})
+    """
+
 def property_catalog(request):
     page = int(request.GET.get('page', 1))
     page_size = 16 if page == 1 else 24
